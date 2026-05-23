d46a0d76493bc4189485641ce014f659b591d406
Abdulhanan535 <rehanazizabdulhanan@gmail.com>
2026-05-24 03:16:08 +0500

    Initial project setup

---
+++ src/com/project/Main.java
@@ -0,0 +1,11 @@
+package com.project;
+import com.project.ui.DashboardGUI;
+/**
+ *
+ * @author Abdul Hanan
+ */
+public class Main {
+    public static void main(String[] args) {
+        new DashboardGUI();
+    }
+}
